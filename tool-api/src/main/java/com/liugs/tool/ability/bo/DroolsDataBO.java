c2135632d277067893d1ebb655d1512ce6993957
liugs <lgschn@qq.com>
2022-01-20 15:12:16 +0800

    规则测试

---
+++ tool-api/src/main/java/com/liugs/tool/ability/bo/DroolsDataBO.java
@@ -0,0 +1,19 @@
+package com.liugs.tool.ability.bo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @ClassName DroolsDataBO
+ * @Description 数据对象
+ * @Author liugs
+ * @Date 2022/1/20 14:42
+ */
+@Data
+public class DroolsDataBO implements Serializable {
+
+    private static final long serialVersionUID = -2568935575652690243L;
+
+    private String value;
+}
--- tool-center/src/main/java/com/liugs/tool/ability/impl/DroolsServiceImpl.java
+++ tool-center/src/main/java/com/liugs/tool/ability/impl/DroolsServiceImpl.java
@@ -1,10 +1,15 @@
 package com.liugs.tool.ability.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.liugs.tool.ability.DroolsService;
+import com.liugs.tool.ability.bo.DroolsDataBO;
 import com.liugs.tool.ability.bo.DroolsReqBO;
 import com.liugs.tool.ability.bo.DroolsRspBO;
+import com.liugs.tool.base.Console;
 import com.liugs.tool.util.ToolRspUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.kie.api.KieBase;
+import org.kie.api.runtime.ClassObjectFilter;
 import org.kie.api.runtime.KieSession;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
@@ -15,6 +20,7 @@ import org.springframework.stereotype.Service;
  * @Author liugs
  * @Date 2022/1/19 17:55
  */
+@Slf4j
 @Service("droolsService")
 public class DroolsServiceImpl implements DroolsService {
 
@@ -26,13 +32,41 @@ public class DroolsServiceImpl implements DroolsService {
 
     @Override
     public DroolsRspBO trigger(DroolsReqBO reqBO) {
+        log.debug("Ultraman trigger：balance type");
         DroolsRspBO retBO = ToolRspUtil.getSuccessRspBo(DroolsRspBO.class);
-        KieSession kieSession = kieBase.newKieSession();
-        kieSession.insert(reqBO);
-        kieSession.fireAllRules();
-        kieSession.dispose();
+        DroolsDataBO dataBO = new DroolsDataBO();
+
+        triggerVirgo(reqBO, dataBO);
+
 
         BeanUtils.copyProperties(reqBO, retBO);
+
         return retBO;
     }
+
+    /**
+     * 描述
+     * @param objects
+     * @return void
+     * @author liugs
+     * @date 2022/1/20 14:48
+     */
+    private void triggerVirgo(Object... objects) {
+        KieSession kieSession = kieBase.newKieSession();
+
+        kieSession.insert(objects[0]);
+        kieSession.setGlobal("dataBO", objects[1]);
+
+        kieSession.fireAllRules();
+
+        for (Object obj : kieSession.getObjects(new ClassObjectFilter(objects[0].getClass()))) {
+            if (100000 == ((DroolsReqBO) obj).getValue()) {
+                Console.show("SUCCESS");
+            }
+        }
+        log.info("Global对象：{}", JSON.toJSONString(objects[1]));
+        kieSession.dispose();
+    }
+
+
 }
